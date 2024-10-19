317de5d958f281f2d63b297ebadea048e973f2c7
mohandElkosh <mokelkosh@gmail.com>
2024-10-19 16:51:14 -0700

    befor stash

---
+++ FoodDelivery.Models/OrderDetailsForML.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodDelivery.Models
+{
+    public class OrderDetailsForML
+    {
+        public float ProductId { get; set; }  // Changed to float
+        public float Quantity { get; set; }   // Changed to float
+        public DateTime OrderDate { get; set; }
+    }
+}
